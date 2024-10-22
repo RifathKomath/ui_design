a4ced7af7e5aa09f297e4f27411835492b531790
rifath <muhammedrifathkk@gmail.com>
2024-10-22 20:54:48 +0400

    updated

--- lib/account_screens/orders.dart
+++ lib/account_screens/orders.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:uidesign/additional/colors.dart';
 
 class Orders extends StatefulWidget {
   const Orders({super.key});
@@ -10,7 +11,7 @@ class Orders extends StatefulWidget {
 
 class _OrdersState extends State<Orders> {
   final String formattedDate = DateFormat('MMMM d, y').format(DateTime.now());
-  // final String formattedTime = DateFormat('HH:mm').format(DateTime(2019, 3, 5, 12, 31));
+  final String formattedTime = DateFormat('HH:mm').format(DateTime.now());
 
   List title = ['Order #345', 'Order #346', 'Order #347'];
   List subTitle = ['Delivered', 'Cancelled', 'Delivered'];
@@ -71,67 +72,144 @@ class _OrdersState extends State<Orders> {
                                   fontSize: 16,
                                   fontWeight: FontWeight.bold),
                             ),
-                            // Text(formattedTime)
+                            const SizedBox(
+                              width: 120,
+                            ),
+                            Text(
+                              formattedTime,
+                              style: const TextStyle(
+                                  color: Colors.orange, fontSize: 15),
+                            )
                           ],
-                        )
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  SizedBox(
+                                      width: 40,
+                                      child:
+                                          Image.asset('assets/image/Group.png')),
+                                  const SizedBox(
+                                    width: 20,
+                                  ),
+                                  const Text(
+                                    'We are picking your items...',
+                                    style: TextStyle(
+                                        color: Color.fromRGBO(109, 56, 5, 1)),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Row(
+                                children: [
+                                  SizedBox(
+                                      width: 50,
+                                      child: Image.asset(
+                                          'assets/image/Scooter Delivery.png')),
+                                  const SizedBox(
+                                    width: 13,
+                                  ),
+                                  const Text(
+                                    'your order is delivering to \nyour location...',
+                                    style: TextStyle(
+                                        color: Color.fromRGBO(109, 56, 5, 1)),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Row(
+                                children: [
+                                  SizedBox(
+                                      width: 50,
+                                      child: Image.asset(
+                                          'assets/image/Group (1).png')),
+                                  const SizedBox(
+                                    width: 15,
+                                  ),
+                                  const Text(
+                                    'your order is recieved...',
+                                    style: TextStyle(
+                                        color: Color.fromRGBO(109, 56, 5, 1)),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
                       ],
                     ),
                   ),
                 ),
                 Center(
-                  child: ListView.separated(
-                      itemBuilder: (context, index) {
-                        return SizedBox(
-                          height: 70,
-                          child: Row(
-                            children: [
-                              const Padding(
-                                padding: EdgeInsets.only(left: 15, right: 15),
-                                child: Icon(
-                                  Icons.card_travel,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListView.separated(
+                        itemBuilder: (context, index) {
+                          return SizedBox(
+                            height: 70,
+                            child: Row(
+                              children: [
+                                const Padding(
+                                  padding: EdgeInsets.only(left: 15, right: 15),
+                                  child: Icon(
+                                    Icons.card_travel,
+                                    color: Colors.orange,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 12,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      '${title[index]}',
+                                      style: const TextStyle(
+                                          color: Color.fromRGBO(109, 56, 5, 1),
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15),
+                                    ),
+                                    Text(
+                                      '${subTitle[index]}',
+                                      style: TextStyle(
+                                          color: index % 2 == 0
+                                              ? Colors.green
+                                              : Colors.red),
+                                    ),
+                                    Text('${date[index]}')
+                                  ],
+                                ),
+                                const SizedBox(
+                                  width: 105,
+                                ),
+                                const Icon(
+                                  Icons.attach_money_rounded,
                                   color: Colors.orange,
                                 ),
-                              ),
-                              const SizedBox(
-                                width: 12,
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    '${title[index]}',
+                                Text('${price[index]}',
                                     style: const TextStyle(
-                                        color: Color.fromRGBO(109, 56, 5, 1),
+                                        color: Colors.orange,
                                         fontWeight: FontWeight.bold,
-                                        fontSize: 15),
-                                  ),
-                                  Text(
-                                    '${subTitle[index]}',
-                                    
-                                  ),
-                                  Text('${date[index]}')
-                                ],
-                              ),
-                              const SizedBox(
-                                width: 105,
-                              ),
-                              const Icon(
-                                Icons.attach_money_rounded,
-                                color: Colors.orange,
-                              ),
-                              Text('${price[index]}',
-                                  style: const TextStyle(
-                                      color: Colors.orange,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20)),
-                            ],
-                          ),
-                        );
-                      },
-                      separatorBuilder: (context, index) {
-                        return const Divider();
-                      },
-                      itemCount: 3),
+                                        fontSize: 20)),
+                              ],
+                            ),
+                          );
+                        },
+                        separatorBuilder: (context, index) {
+                          return const Divider();
+                        },
+                        itemCount: 3),
+                  ),
                 )
               ]))
             ],
--- lib/additional/colors.dart
+++ lib/additional/colors.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 TextStyle smallParagraph(){
     return const TextStyle(
@@ -6,4 +7,6 @@ TextStyle smallParagraph(){
         
     );
 }
+
+
 
